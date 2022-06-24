1eef99429ea8e498985feb63472b90d776c7881f
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-24 16:58:56 +0530

    first commit

---
+++ JavaPractice/src/datatypes/PrimitiveTypes.java
@@ -0,0 +1,9 @@
+package datatypes;
+
+import java.sql.SQLOutput;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        System.out.println("Primitive Types");
+    }
+}
