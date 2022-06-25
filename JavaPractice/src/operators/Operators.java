8851b1bcfefb37fb5ee8aaa00a43dc9eaa471f14
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-25 11:48:51 +0530

    adding arithmetic operators file

--- JavaPractice/src/operators/Operators.java
+++ JavaPractice/src/operators/Operators.java
@@ -1,4 +1,11 @@
 package operators;
 
 public class Operators {
+    public static void main(String[] args) {
+        System.out.println(-5+8*6);
+        System.out.println((55+9)%9);
+        System.out.println(20+ -3*5 / 8);
+        System.out.println(5+15/3*2-8%3);
+        long l1 = 9223372036854775807l;
+    }
 }
