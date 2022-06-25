a14ec57d5fc27aac92d899d31215e561ff2ea0f4
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-25 11:45:17 +0530

    pushing Variable initiated file

---
+++ JavaPractice/src/rough/Variable.java
@@ -0,0 +1,11 @@
+package rough;
+
+public class Variable {
+    public static void main(String[] args) {
+        String myName ;
+        String myAddress ;
+        myName = "Devesh Kumar";
+        myAddress = " Patna";
+        System.out.println(myName+ myAddress);
+    }
+}
