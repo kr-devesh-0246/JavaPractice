0873626d361ec2f91e3dfec054590a59f3b524ba
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-25 12:06:24 +0530

    added 1 new line for float data type

--- JavaPractice/src/operators/Operators.java
+++ JavaPractice/src/operators/Operators.java
@@ -7,5 +7,8 @@ public class Operators {
         System.out.println(20+ -3*5 / 8);
         System.out.println(5+15/3*2-8%3);
         long l1 = 9223372036854775807l;
+        float f1 = 300.00F;
+        System.out.println(l1);
+        System.out.println(f1);
     }
 }
