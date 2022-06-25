45802bc8f19852f0ad04d186ab94b936bac39cf0
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-25 13:21:55 +0530

    Adding concatination file

---
+++ JavaPractice/src/datatypes/StringConcatination.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class StringConcatination {
+    public static void main(String[] args) {
+        String part1 = "Devesh kumar";
+        String part2 = "is new to Java programming";
+        String full = part1 + " " + part2;
+
+        System.out.println(full);
+    }
+
+}
