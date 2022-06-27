b97ee98ef989ea8c87a2593c5d51baccc811a3eb
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-27 21:55:56 +0530

    Made user input easy

--- JavaPractice/src/operators_conditional/EvenOdd.java
+++ JavaPractice/src/operators_conditional/EvenOdd.java
@@ -5,6 +5,7 @@ public class EvenOdd{
     public static void main(String[] args) {
         //read an int
         Scanner s = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
         int n = s.nextInt();
         if(n % 2 == 0)
             System.out.println("Even");
