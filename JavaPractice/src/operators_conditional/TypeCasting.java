a3fbd761233c6f60a4be07bc1313bee51153ca7e
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-27 20:05:33 +0530

    Adding new file

---
+++ JavaPractice/src/operators_conditional/TypeCasting.java
@@ -0,0 +1,22 @@
+package operators_conditional;
+
+public class TypeCasting {
+    public static void main(String[] args){
+        //happens when converting a data type to another
+        //It is of two types: Implicit and Explicit
+        //Implicit-narrow to wider range data type(When conversion does not lose data), (Happens automatically by java)
+        //Explicit-wider to narrower range data type(When it might lose some data), (Programmers does it)
+        //Range order:- long > float > double > int > short > byte (Check the order again)
+//Implicit
+        double d1 = 4;     // int -> double
+        double d2 = 3.0f;  // float -> double
+        long l1 = 100;     // int -> long
+//Explicit
+        //(new data type) expression
+        int i1 = (int) 4.5f;
+        int i2 = (int) 9L ;
+        float f1 = (float) 4.5;
+
+    }
+
+}
