e609d4b8d0aea11709c8e0e89fafe43062ca5e31
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-27 22:57:15 +0530

    Solved EvenOdd using switch statement

--- JavaPractice/src/operators_conditional/EvenOdd.java
+++ JavaPractice/src/operators_conditional/EvenOdd.java
@@ -4,12 +4,27 @@ import java.util.Scanner;
 public class EvenOdd{
     public static void main(String[] args) {
         //read an int
+//        Scanner s = new Scanner(System.in);
+//        System.out.print("Enter an integer: ");
+//        int n = s.nextInt();
+//        if(n % 2 == 0)
+//            System.out.println("Even");
+//        if(n % 2 != 0)
+//            System.out.println("Odd");
+        //Method-2(Using Switch Statements
+        System.out.print("Enter an int: ");
         Scanner s = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
-        int n = s.nextInt();
-        if(n % 2 == 0)
-            System.out.println("Even");
-        if(n % 2 != 0)
-            System.out.println("Odd");
+        int num = s.nextInt();
+
+        switch (num % 2){
+            case 0 :
+                System.out.println("Even");
+                break;
+
+            case 1 :
+                System.out.println("Odd");
+                break;
+
+        }
     }
 }
