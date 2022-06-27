81590a0bb9e15734a50d49bebb3f4f88eb56c544
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-27 22:45:40 +0530

    adding SwitcchStatements

---
+++ JavaPractice/src/operators_conditional/SwitchStatements.java
@@ -0,0 +1,20 @@
+package operators_conditional;
+
+public class SwitchStatements {
+    public static void main(String[] args) {
+        int num = 10;
+        switch (num) {
+            case 0:
+                System.out.println("0");
+                break;
+            case 10:
+                System.out.println("10");
+                break;
+            case 20:
+                System.out.println("20");
+                break;
+            default:
+                System.out.println("None of the above");
+        }
+    }
+}
