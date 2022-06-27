0bd176f2e17ec2eed9ec6ebb7e83b6fd22cb936b
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-27 22:22:37 +0530

    Adding Nested if else example

---
+++ JavaPractice/src/operators_conditional/NestedIf.java
@@ -0,0 +1,14 @@
+package operators_conditional;
+
+public class NestedIf{
+    public static void main(String[] args) {
+        int i = 0;
+        if (i > 10)
+            System.out.println(">10");
+        else if (i == 10)
+            System.out.println("=10");
+        else
+            System.out.println("<10");
+
+    }
+}
