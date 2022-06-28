ac2e9a1c9ecb2c2c6a152ef88686adb87d838441
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-28 14:54:42 +0530

    Made a new file

---
+++ JavaPractice/src/loops/WhileLoop.java
@@ -0,0 +1,19 @@
+package loops;
+import java.util.Scanner;
+public class WhileLoop {
+    public static void main(String[] args) {
+        //read and input until user enter valid input(range(1, 10))
+        System.out.print("Enter an int between 1 and 10: ");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        while (num < 1 || num > 10){
+
+            System.out.print("Enter int in range(1,10): ");
+            num = s.nextInt();
+        }
+        System.out.println("Ok Thank you, it's a valid input:)");
+    }
+
+
+}
