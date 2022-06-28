54c898c7a6339f3c55ad22c03bf58e5d5cdb4cc6
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-28 14:14:54 +0530

    The logic was improved

--- JavaPractice/src/operators_conditional/LuckyNumbers.java
+++ JavaPractice/src/operators_conditional/LuckyNumbers.java
@@ -1,5 +1,4 @@
 package operators_conditional;
-
 import java.util.Scanner;
 
 public class LuckyNumbers {
@@ -7,13 +6,20 @@ public class LuckyNumbers {
         //Lucky no - ABCD is lucky if A+B == C+D
         System.out.print("Enter a four digit integer: ");
         Scanner s = new Scanner(System.in);
-        String num = s.next();
-        int a = num.charAt(0);
-        int b = num.charAt(1);
-        int c = num.charAt(2);
-        int d = num.charAt(3);
+        int num = s.nextInt();
+        //convert the string to num for if condition
 
-        System.out.println(a+b == c+d ? "Lucky number" : "Not a lucky number");
+        if (  num > 9999 ||  num < 1000 ) {
+            System.out.println("Enter a 4 digit int: ");
+        }
+        else {
+            int d = (num / 1) % 10;
+            int c = (num / 10) % 10;
+            int b = (num / 100) % 10;
+            int a = (num / 1000) % 10;
 
+            System.out.println(a+b == c+d ? "Lucky number" : "Not a lucky number");
+
+        }
     }
 }
