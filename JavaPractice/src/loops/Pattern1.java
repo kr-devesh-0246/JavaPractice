6d945ba8e15cb031f498d3dcbdf8c6f447a24ce0
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-28 21:25:31 +0530

    Adding pattern of Inverted triangle

--- JavaPractice/src/loops/Pattern1.java
+++ JavaPractice/src/loops/Pattern1.java
@@ -2,15 +2,14 @@ package loops;
 
 public class Pattern1 {
     public static void main (String[] args) {
-        int i = 5;
-        int j = 5;
-        for (i = 5; i >= 1; i--){
-//            System.out.println("outer loop");
-            for (j = 5; j >= i; j-- ){
-//                System.out.println("inner loop");
-                System.out.print("*");
+        // * * *
+        // * *
+        // *
+        for (int i = 1; i <=3; i++) {
+            for (int j = 3; j >= i; j-- ) {
+                System.out.print("*" + " ");
             }
             System.out.println();
         }
+        }
     }
-}
