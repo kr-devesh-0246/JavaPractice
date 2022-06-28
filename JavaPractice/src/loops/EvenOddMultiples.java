5d999a13ed016f88dd2ffb389398a6a973b7f126
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-28 21:36:54 +0530

    made a new file

---
+++ JavaPractice/src/loops/EvenOddMultiples.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class EvenOddMultiples {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
