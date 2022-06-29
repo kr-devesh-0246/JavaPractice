247c8ef4ee2880b0750c0ed61b343e4378653edb
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-29 19:17:28 +0530

    Added a new pattern

---
+++ JavaPractice/src/loops/Pattern4.java
@@ -0,0 +1,25 @@
+//     *
+//    ***
+//   *****
+//  *******
+
+package loops;
+import java.util.Scanner;
+public class Pattern4 {
+    public static void main(String[] args) {
+        System.out.print("Enter an integer n: ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 0; i <= n; i++){
+
+            for (int j = 1; j <= n-i ; j++){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2*i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
