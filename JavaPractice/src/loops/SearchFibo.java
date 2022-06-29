04d5a6ac6be4f1d1d0d65ec13dc7dfa561676c49
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-29 16:15:41 +0530

    Improved the logic

--- JavaPractice/src/loops/SearchFibo.java
+++ JavaPractice/src/loops/SearchFibo.java
@@ -11,18 +11,20 @@ public class SearchFibo {
         int a = 1;
         int b = 1;
         int c = a+b;
-//        System.out.print(a);
-//        System.out.print(" ");
-//        System.out.print(b);
-//        System.out.print(" ");
-        for (int i = 0; i <= n-3; i++){
-//            System.out.print(c);
-//            System.out.print(" ");
-            a = b;
-            b = c;
-            c = a + b;
-        }
-        System.out.println(n + "th number of the fibonacci sequence: " + c);
 
+        if (n == 0)
+            System.out.println("1st element of fino: " + a);
+
+        else if (n == 1)
+            System.out.println("2nd element of fibo: " + b);
+
+        else if (n > 1) {
+            for (int i = 1; i <= n - 2; i++) {
+                a = b;
+                b = c;
+                c = a + b;
+            }
+            System.out.println(n + "th number of the fibonacci sequence: " + c);
+        }
     }
 }
