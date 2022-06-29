0de24bda628e862ca50db66b806663d31c4fab16
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-29 18:00:01 +0530

    Adding a new file for reversing a string

---
+++ JavaPractice/src/loops/ReverseString.java
@@ -0,0 +1,18 @@
+package loops;
+import java.util.Scanner;
+public class ReverseString {
+    public static void main(String[] args){
+
+        System.out.print("Enter a string: ");
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+
+        //take reverse indexing and store or print the result once
+        String rev = "";
+        for (int i = str.length()-1; i >= 0; i--){
+            rev += str.charAt(i);
+        }
+        System.out.println(rev);
+
+    }
+}
