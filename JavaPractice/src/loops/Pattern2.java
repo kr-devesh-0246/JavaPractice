13496fd7f25ca9d3d2908325487594e0cb31cd2e
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-29 18:33:48 +0530

    Made a new pattern

---
+++ JavaPractice/src/loops/Pattern2.java
@@ -0,0 +1,16 @@
+package loops;
+import java.util.Scanner;
+public class Pattern2 {
+    public static void main(String[] args) {
+        System.out.print("Enter n: ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j<=i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
