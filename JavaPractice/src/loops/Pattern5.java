69c686c4010faab0f868a32d90c6d701b291d463
Devesh Kumar <devesh@Mercy.localdomain>
2022-06-29 19:39:41 +0530

    Adding few unfinished files

---
+++ JavaPractice/src/loops/Pattern5.java
@@ -0,0 +1,22 @@
+//    *
+//   * *
+//  *   *
+// *     *
+//*********
+package loops;
+import java.util.Scanner;
+public class Pattern5 {
+    public static void main(String[] args) {
+
+        System.out.print("Enter an int: ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for(int j = 1; j <=  )
+
+            System.out.println();
+        }
+    }
+}
---
+++ JavaPractice/src/loops/Pattern7.java
@@ -0,0 +1,15 @@
+package loops;
+import java.util.Scanner;
+public class Pattern7 {
+    public static void main (String[] args){
+
+        System.out.print("Enter n: ");
+        Scanner s = new Scanner(System.in);
+        int n =  s.nextInt();
+
+        for (int i = 1; i <= n; i++){
+
+            System.out.println();
+        }
+    }
+}
