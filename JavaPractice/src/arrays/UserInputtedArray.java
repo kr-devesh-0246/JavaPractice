9d0c77358c51d1773dff654e1a8505eeeb893dc5
Devesh Kumar <devesh@Mercy.localdomain>
2022-07-01 18:46:15 +0530

    array on n integers, filled by user enterred integers

---
+++ JavaPractice/src/arrays/UserInputtedArray.java
@@ -0,0 +1,22 @@
+// write a program that fills an array with n integers entered by user
+package arrays;
+import java.util.Scanner;
+public class UserInputtedArray {
+    public static void main(String[] args) {
+
+        System.out.print("Enter n: ");
+        Scanner s = new Scanner(System.in);
+        Scanner inp = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++)
+
+            result[i] = inp.nextInt();
+
+        //Printing the array
+        for (int i = 0; i < n; i++)
+            System.out.print(result[i] + " ");
+    }
+}
