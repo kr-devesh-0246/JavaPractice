8f8ce6421f6d1c054bcc822d96c3dd93fdb7f4cd
Devesh Kumar <devesh@Mercy.localdomain>
2022-07-01 18:25:03 +0530

    Adding few examples in the file

--- JavaPractice/src/arrays/VariableLenArgList.java
+++ JavaPractice/src/arrays/VariableLenArgList.java
@@ -6,6 +6,15 @@ public class VariableLenArgList {
         System.out.println(sum(1, 9, -1)); // 9
         System.out.println(sum(1, 3, -3, 1)); // 2
         System.out.println(sum(0, 1, 99, -1, 2)); // 101
+
+        //Few more examples
+        int[] numbers = {1, 7, -2};
+        System.out.println(sum(numbers));
+
+        //We can do it anonymously
+        System.out.println(sum(new int[] {1,2,3,4}));
+
+        //Upto here we have learned only 1 variable parameter list.
     }
     //3-dots refer to variable arguments
     //the numbers is the array now.
