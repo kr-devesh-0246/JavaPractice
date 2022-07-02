c8f08d36968a6a69f1297a01e998702f251da6c8
Devesh Kumar <devesh@Mercy.localdomain>
2022-07-02 09:54:52 +0530

    learnt how sorting is done

---
+++ JavaPractice/src/arrays/ArrayClassSorting.java
@@ -0,0 +1,11 @@
+package arrays;
+import java.util.Arrays;
+public class ArrayClassSorting {
+    public static void main (String[] args) {
+        // initialize and declare an array
+        int[] numbers = {1, 2, 3, 4, -6, -5, -4, -3, 0};
+        // sorting that array
+        Arrays.sort(numbers); // {-6, -5, -4, -3, 0, 1, 2, 3, 4}
+    }
+
+}
