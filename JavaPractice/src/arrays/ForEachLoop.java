ec4d453f5f953ef1e7546c018f73bb66a539e610
Devesh Kumar <devesh@Mercy.localdomain>
2022-07-09 20:50:16 +0530

    dash dash message

---
+++ JavaPractice/src/arrays/ForEachLoop.java
@@ -0,0 +1,20 @@
+package arrays;
+import java.util.ArrayList;
+public class ForEachLoop {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(0);
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        System.out.println(integers);
+
+        for (:
+             ) {
+            
+        }() {
+
+        }
+    }
+}
